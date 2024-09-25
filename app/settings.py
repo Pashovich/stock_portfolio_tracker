c5225b6c2abe68e00448517cbd1002dde6e9f4a3
pavelSidorovMonetha <114472597+pavelSidorovMonetha@users.noreply.github.com>
2024-09-25 16:30:45 +1000

    fix

--- app/settings.py
+++ app/settings.py
@@ -95,11 +95,22 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+default_db = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.environ.get('POSTGRES_DB'),
+    'USER': os.environ.get('POSTGRES_USER'),
+    'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+    'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+    'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+}
+
+if os.getenv('DEBUG'):
+    default_db = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+DATABASES = {
+    'default' : default_db
 }
 
 
